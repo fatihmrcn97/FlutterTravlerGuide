1766d1e2403096d7da4f8f3134bc8c3f81087d44
fatihmrcn97 <fatihmercan7991@gmail.com>
2020-02-07 18:29:21 +0300

    Nice interface

---
+++ lib/widgets/hotel_carousel.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_travel_ui_starter/models/destination_model.dart';
+import 'package:flutter_travel_ui_starter/models/hotel_model.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class HotelCarousel extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text(
+                'Exclusive Hotels',
+                style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1.5),
+              ),
+              GestureDetector(
+                  onTap: () => print("ssssa"),
+                  child: Text(
+                    'See All ',
+                    style: TextStyle(
+                        color: Theme.of(context).primaryColor,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                        letterSpacing: 1),
+                  ))
+            ],
+          ),
+        ),
+        Container(
+          height: 300,
+          //color: Colors.red,
+          child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: hotels.length,
+              itemBuilder: (BuildContext context, int index) {
+                Hotel hotel = hotels[index];
+                return Container(
+                  margin: EdgeInsets.all(10.0),
+                  //color: Colors.red,
+                  width: 240,
+                  child: Stack(
+                    alignment: Alignment.topCenter,
+                    children: <Widget>[
+                      Positioned(
+                        bottom: 15,
+                        child: Container(
+                          height: 120,
+                          width: 250,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          child: Padding(
+                            padding: EdgeInsets.all(10.0),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: <Widget>[
+                                Text(
+                                  hotel.name,
+                                  style: TextStyle(
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.w600,
+                                      letterSpacing: 1.2),
+                                ),
+                                SizedBox(height: 2.0,),
+                                Text(
+                                  hotel.address,
+                                  style: TextStyle(color: Colors.grey),
+                                ),
+                                SizedBox(height: 2.0,),
+                                Text('\$${hotel.price} / night',style: TextStyle(fontSize: 18.0,fontWeight: FontWeight.w600),),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(20.0),
+                            boxShadow: [
+                              BoxShadow(
+                                  color: Colors.black26,
+                                  offset: Offset(0.0, 2.0),
+                                  blurRadius: 6.0)
+                            ]),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(20.0),
+                          child: Image(
+                            height: 180.0,
+                            width: 220.0,
+                            image: AssetImage(hotel.imageUrl),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              }),
+        ),
+      ],
+    );
+  }
+}
